1009db5d6b6437804df81f7ccac03ee8db85d5dc
Karen Calisaya <karenmcalisaya@gmail.com>
2022-03-11 13:56:43 -0300

    proyect sixapples prueba

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const path = require('path');
+
+app.use(express.static('public'));
+
+app.listen(PORT, () => console.log(`Servidor escuchando en el puerto ${PORT}
+http://localhost:${PORT}`));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/home.html'))
+});
+
+app.get('/quienessomos', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/quienessomos.html'))
+})
+
+app.get('/carrusel', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/carrusel.html'))
+})
+
+app.get('/carrito', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/carrito.html'))
+})
+
+app.get('/catalogo', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/catalogo.html'))
+})
