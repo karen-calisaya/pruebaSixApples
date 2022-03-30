dd88c9231a4816ffc3c8291798be0f4f1cedae53
Karen Calisaya <karenmcalisaya@gmail.com>
2022-03-30 16:26:45 -0300

    practice ejs-router-controller

--- src/app.js
+++ src/app.js
@@ -3,14 +3,25 @@ const app = express();
 const PORT = 3000;
 const path = require('path');
 
+/* enrutadores */
+const indexRouter = require('./routes/indexRouter');
+
 app.use(express.static('public'));
 
 app.listen(PORT, () => console.log(`Servidor escuchando en el puerto ${PORT}
 http://localhost:${PORT}`));
 
-app.get('/', (req, res) => {
+/* views config */
+app.set('view engine', 'ejs');
+app.set('views', 'src/views'); 
+
+/* middlewares de rutas*/
+app.use('/', indexRouter);
+
+
+/* app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/home.html'))
-});
+}); */
 
 app.get('/quienessomos', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/quienessomos.html'))
