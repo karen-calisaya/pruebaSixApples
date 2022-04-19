afd15becf9003f3888ff689b6f3cfff342c07d99
Karen Calisaya <karenmcalisaya@gmail.com>
2022-04-18 21:56:35 -0300

    validations array

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const userController = require("../controllers/userController");
 const router = express.Router();
-const productController = require("../controllers/userController");
+const { check } = require('express-validator')
+
+const validaciones = [
+    check('name')
+        .notEmpty().withMessage('Este campo es obligatorio').bail(),
+    check('email')
+        .notEmpty().withMessage('Este campo es obligatorio').bail()
+        .isEmail().withMessage('Debe ser un e-mail válido'),
+    check('password')
+        .notEmpty().withMessage('Este campo es obligatorio')
+        .isLength({min: 5})
+]
 
 /* Ruta para mostrar los productos */
 router.get('/', userController.login);
 router.get('/perfil', userController.profile);
-router.get('/registro', userController.register);
+router.get('/registro', validaciones ,userController.register);
 
 module.exports = router;
